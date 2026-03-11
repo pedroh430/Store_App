2fb0af83cdc8420f2db2426c688c53e45988a958
pedro <pedrobldap41@gmail.com>
2026-03-11 18:58:25 -0300

    .

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get("/api/health" , (req, res) => {
-    res.status(200).json({ message: "success"});
-});
-
-app.listen(3000, () => console.log("server is running "));
