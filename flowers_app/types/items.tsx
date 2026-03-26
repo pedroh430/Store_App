95c9625c20d43e692931123aeaddf7323ca4a80c
pedro <pedrobldap41@gmail.com>
2026-03-26 20:47:28 -0300

    create items for teste in app

---
+++ flowers_app/types/items.tsx
@@ -0,0 +1,8 @@
+
+export const items = [
+    {name: "preto", price: 10.00, image: require("../assets/images/Logo.png") },
+    {name: "preto", price: 10.00, image: require("../assets/images/Logo.png") },
+    {name: "rosa", price: 10.00, image: require("../assets/images/Logo.png") },
+    {name: "rosa", price: 10.00, image: require("../assets/images/Logo.png") },
+    {name: "rosa", price: 10.00, image: require("../assets/images/icon.png") }
+];
