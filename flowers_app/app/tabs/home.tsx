e4b3c08dce8e7023ae16b8029d40a64ac6d58630
pedro <pedrobldap41@gmail.com>
2026-03-26 20:46:40 -0300

    add items test in flatlist

--- flowers_app/app/tabs/home.tsx
+++ flowers_app/app/tabs/home.tsx
@@ -1,8 +1,8 @@
 import ProductCard from "@/components/ProductCard";
 import { Button } from "@react-navigation/elements";
 import { Stack } from "expo-router";
-import { ScrollView, StyleSheet, View, Text } from "react-native";
-
+import { ScrollView, StyleSheet, View, Text, FlatList } from "react-native";
+import { items } from "@/types/items";
 
 export default function HomeScreen() {
    
@@ -10,21 +10,17 @@ export default function HomeScreen() {
 
   return (
 
-    <View>
-
+    <FlatList 
      
+      data={items}
+      numColumns={2}
+      columnWrapperStyle={styles.row}
+      contentContainerStyle={styles.conteine}
+      style={styles.background}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => <ProductCard item={item} />}
+    />
 
-
-
-      
-
-    
-
-
-
-    </View>
-
-    
 
     
   );
@@ -33,11 +29,24 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
 
+
+  row:{
+    gap: 12,
+    padding: 12,
+
+  },
+
   conteine: {
   flexDirection: "row",
   flexWrap: "wrap",
   justifyContent: "space-between",
   paddingBottom: 20,
+  padding: 16,
+  gap: 16,
+  
+  
+
+  
 },
 
  background:{
